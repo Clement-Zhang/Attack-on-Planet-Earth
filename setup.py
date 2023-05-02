6566051a2b78b7b5ce811d0ee4752821ee51dc69
Clement-Zhang <zhangclement947@yahoo.ca>
2023-05-02 15:31:44 -0400

    Revert "rename the game"
    This reverts commit 842e9fc9651e769dc957a22ec6933fe059b931a0.

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ import constants.static
 pygame.init()
 screen = pygame.display.set_mode(
     (constants.static.WIDTH, constants.static.HEIGHT))
-pygame.display.set_caption("Attack on Planet Earth")
+pygame.display.set_caption("Extra Space Invaders")
 clock = pygame.time.Clock()
 running = 1
