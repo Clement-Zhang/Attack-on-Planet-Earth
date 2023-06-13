2e4249e864bb7f0eb63b047ba8fa28542117c5b0
Clement-Zhang <zhangclement947@yahoo.ca>
2023-06-13 15:25:57 -0400

    commit to using better art

--- constants/dynamic.py
+++ constants/dynamic.py
@@ -4,7 +4,7 @@ import constants.static
 
 GAME_FOLDER = os.path.join(os.path.dirname(__file__), '..')
 PLAYER_FOLDER = os.path.join(
-    GAME_FOLDER, "custom art/player")
+    GAME_FOLDER, "art/player")
 ENEMY_FOLDER = os.path.join(
-    GAME_FOLDER, "custom art/enemy")
-PROJECTILE_FOLDER = os.path.join(GAME_FOLDER, "custom art/projectile")
+    GAME_FOLDER, "art/enemy")
+PROJECTILE_FOLDER = os.path.join(GAME_FOLDER, "art/projectile")
